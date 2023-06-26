b360d70196a85c251dbb0f6bbd6d74e591160948
Guilin <guilinw98@gmail.com>
2023-06-26 16:52:35 +0800

    OIO

--- s1-bio/src/main/java/com/stars/s1bio/ServerSocketTest.java
+++ s1-bio/src/main/java/com/stars/s1bio/ServerSocketTest.java
@@ -6,34 +6,50 @@ import java.net.Socket;
 
 public class ServerSocketTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //创建一个新的 ServerSocket，用以 监听指定端口上的连接请求
-        ServerSocket serverSocket;
-        PrintWriter out = null;
-        try {
-            serverSocket = new ServerSocket(5001);
-            //对 accept () 方法的调 用将被阻塞，直到一 个连接建立
-            Socket clientSocket = serverSocket.accept();
-            //这些流对象都派生于该套接字的流对象
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            out = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()), true);
+        final ServerSocket serverSocket = new ServerSocket(5001);
+        ;
 
-            String requestStr, responseStr;
-            while ((requestStr = in.readLine()) != null) {
-                System.out.println(requestStr);
-                if ("Done".equals(requestStr))
-                    break;
-                responseStr = "response:" + requestStr;
-                //服务器的响应被,发送给了客户端
-                out.println(responseStr);
+        try {
+            for (; ; ) {
+
+                //对 accept () 方法的调 用将被阻塞，直到一 个连接建立
+                final Socket clientSocket = serverSocket.accept();
+                System.out.println("Accepted connection from " + clientSocket);
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        PrintWriter out = null;
+                        try {
+                            //这些流对象都派生于该套接字的流对象
+                            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                            out = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()), true);
+                            String requestStr, responseStr;
+                            while ((requestStr = in.readLine()) != null) {
+                                System.out.println(requestStr);
+                                if ("Done".equals(requestStr))
+                                    break;
+                                responseStr = "response:" + requestStr;
+                                //服务器的响应被,发送给了客户端
+                                out.println(responseStr);
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } finally {
+                            try {
+                                clientSocket.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            if (out != null)
+                                out.close();
+                        }
+                    }
+                }).start();
             }
         } catch (IOException e) {
-            throw new RuntimeException(e);
-        } finally {
-            if (out != null)
-                out.close();
+            e.printStackTrace();
         }
-
-
     }
 }
--- s1-bio/src/main/java/com/stars/s1bio/SocketClientTest.java
+++ s1-bio/src/main/java/com/stars/s1bio/SocketClientTest.java
@@ -8,29 +8,33 @@ public class SocketClientTest {
 
     public static void main(String[] args) {
 
-        Socket socket = null;
-        PrintWriter out = null;
-        BufferedReader in = null;
-        InetAddress inet = null;
-        try {
-            inet = InetAddress.getByName("127.0.0.1");
-            socket = new Socket(inet, 5001);
-            //输出流 os对象初始化
-            out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
-            out.println("who am I ?");
-            out.println("say hi ");
-            out.println("Done");
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            String line;
-            while ((line = in.readLine()) != null)
-                System.out.println(line);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } finally {
-            if (out != null)
-                out.close();
+        for (int i = 0; i < 5; i++) {
+            Socket socket = null;
+            PrintWriter out = null;
+            BufferedReader in = null;
+            InetAddress inet = null;
+            try {
+                inet = InetAddress.getByName("127.0.0.1");
+                socket = new Socket(inet, 5001);
+                //输出流 os对象初始化
+                out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
+                out.println("who am I ?");
+                out.println("say hi "+i);
+                out.println("Done");
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String line;
+                while ((line = in.readLine()) != null)
+                    System.out.println(line);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } finally {
+                if (out != null)
+                    out.close();
 
+            }
         }
+
+
 
     }
 }
